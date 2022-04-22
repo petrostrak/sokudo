443b8eeccbe6d5d6d461a2ddfc280829bc43097c
petrostrak <pit.trak@gmail.com>
2022-04-22 19:00:14 +0300

    Add GoPage renderer method

--- render/render.go
+++ render/render.go
@@ -1,6 +1,8 @@
 package render
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 	"strings"
 )
@@ -28,7 +30,27 @@ type TemplateData struct {
 func (s *Render) Page(w http.ResponseWriter, r *http.Request, view string, variables, data interface{}) error {
 	switch strings.ToLower(s.Renderer) {
 	case "go":
+		return s.GoPage(w, r, view, data)
 	case "jet":
 	}
+	return nil
+}
+
+func (s *Render) GoPage(w http.ResponseWriter, r *http.Request, view string, data interface{}) error {
+	tmpl, err := template.ParseFiles(fmt.Sprintf("%s/views/%s.page.tmpl", s.RootPath, view))
+	if err != nil {
+		return err
+	}
+
+	td := &TemplateData{}
+	if data != nil {
+		td = data.(*TemplateData)
+	}
+
+	err = tmpl.Execute(w, &td)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
