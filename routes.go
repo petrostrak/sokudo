a36475474ba0b724858c9bd77db45b3459895d36
petrostrak <pit.trak@gmail.com>
2022-04-23 22:08:17 +0300

    Add session middleware

--- routes.go
+++ routes.go
@@ -1,7 +1,6 @@
 package sokudo
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -16,8 +15,7 @@ func (s *Sokudo) routes() http.Handler {
 		mux.Use(middleware.Logger)
 	}
 	mux.Use(middleware.Recoverer)
-
-	mux.Get("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, "Welcome to 速度!") })
+	mux.Use(s.SessionLoad)
 
 	return mux
 }
