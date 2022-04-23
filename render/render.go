efba7de2e672f610b62055156bbdca884395ff0e
petrostrak <pit.trak@gmail.com>
2022-04-23 17:12:55 +0300

    Add more coverage for render package

--- render/render.go
+++ render/render.go
@@ -1,6 +1,7 @@
 package render
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"log"
@@ -37,8 +38,9 @@ func (s *Render) Page(w http.ResponseWriter, r *http.Request, view string, varia
 		return s.GoPage(w, r, view, data)
 	case "jet":
 		return s.JetPage(w, r, view, variables, data)
+	default:
 	}
-	return nil
+	return errors.New("no rendering engine specified")
 }
 
 // GoPage renders a standard Go template
--- render/render_test.go
+++ render/render_test.go
@@ -37,4 +37,41 @@ func TestRender_Page(t *testing.T) {
 	if err == nil {
 		t.Error("error rendering non-existent jet template", err)
 	}
+
+	testRenderer.Renderer = ""
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err == nil {
+		t.Error("no error returned while rendering with invalid renderer specified", err)
+	}
+}
+
+func TestRender_GoPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/url", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	testRenderer.Renderer = "go"
+	testRenderer.RootPath = "./testdata"
+
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error("error rendering page", err)
+	}
+}
+
+func TestRender_JetPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/url", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	testRenderer.Renderer = "jet"
+
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error("error rendering page", err)
+	}
 }
