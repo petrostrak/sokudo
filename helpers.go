119676930079d3506bbaf32eafbf503a8e5fe8bf
petrostrak <pit.trak@gmail.com>
2022-04-29 15:19:55 +0300

    Add RandomString

--- helpers.go
+++ helpers.go
@@ -1,8 +1,27 @@
 package sokudo
 
 import (
+	"crypto/rand"
 	"os"
 )
+
+const (
+	randomString = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0987654321_+"
+)
+
+// RandomString generates a random string length n from values in
+// const randomString.
+func (s *Sokudo) RandomString(n int) string {
+	str, r := make([]rune, n), []rune(randomString)
+
+	for i := range str {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		str[i] = r[x%y]
+	}
+
+	return string(str)
+}
 
 func (s *Sokudo) CreateDirIfNotExists(path string) error {
 	const mode = 0755
