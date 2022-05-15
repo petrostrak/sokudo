4e2619cbe258f0f11b29b5aa57dffd1c82e5706a
petrostrak <pit.trak@gmail.com>
2022-05-15 19:55:25 +0300

    Update migrate down command

--- cmd/cli/auth.go
+++ cmd/cli/auth.go
@@ -17,7 +17,7 @@ func doAuth() error {
 		exitGracefully(err)
 	}
 
-	err = copyDataToFile([]byte("drop table if exists users cascade"), downFile)
+	err = copyDataToFile([]byte("drop table if exists users cascade; drop table if exists tokens cascade; drop table if exists remember_tokens;"), downFile)
 	if err != nil {
 		exitGracefully(err)
 	}
