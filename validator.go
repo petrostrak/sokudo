a6a6520b51590bc6c5858281e44bb5000f8945d3
petrostrak <pit.trak@gmail.com>
2022-05-17 16:28:34 +0300

    Add AddError method

--- validator.go
+++ validator.go
@@ -17,3 +17,9 @@ func (s *Sokudo) Validator(data url.Values) *Validation {
 func (v *Validation) Valid() bool {
 	return len(v.Errors) == 0
 }
+
+func (v *Validation) AddError(key, message string) {
+	if _, exists := v.Errors[key]; !exists {
+		v.Errors[key] = message
+	}
+}
