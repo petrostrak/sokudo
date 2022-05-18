a3ce2f874a807f677a1f7012315d1641d7c1d602
petrostrak <pit.trak@gmail.com>
2022-05-18 14:19:58 +0300

    Fix bug in DownloadFile handler

--- response-utils.go
+++ response-utils.go
@@ -59,7 +59,7 @@ func (s *Sokudo) DownloadFile(w http.ResponseWriter, r *http.Request, pathToFile
 	fullPath := path.Join(pathToFile, fileName)
 	fileToServe := filepath.Clean(fullPath)
 
-	w.Header().Set("Content-Type", fmt.Sprintf("attachment; file=\"%s\"", fileName))
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; file=\"%s\"", fileName))
 	http.ServeFile(w, r, fileToServe)
 
 	return nil
