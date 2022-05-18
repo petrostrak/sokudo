4d84249dbdd9af699145544532de9efbfbc3c625
petrostrak <pit.trak@gmail.com>
2022-05-18 13:44:42 +0300

    Add WriteXML

--- response-utils.go
+++ response-utils.go
@@ -2,6 +2,7 @@ package sokudo
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"net/http"
 )
 
@@ -18,6 +19,29 @@ func (s *Sokudo) WriteJson(w http.ResponseWriter, status int, data interface{}, 
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Sokudo) WriteXML(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := xml.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/xml")
 	w.WriteHeader(status)
 
 	_, err = w.Write(out)
