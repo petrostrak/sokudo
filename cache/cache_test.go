19549ca7b1d246fd50828babd4607b2cc2e9f73d
petrostrak <pit.trak@gmail.com>
2022-05-19 12:49:19 +0300

    Add TestRedisCache_Has

---
+++ cache/cache_test.go
@@ -0,0 +1,33 @@
+package cache
+
+import "testing"
+
+func TestRedisCache_Has(t *testing.T) {
+	err := testRedisCache.Forget("foo")
+	if err != nil {
+		t.Error(err)
+	}
+
+	inCache, err := testRedisCache.Has("foo")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if inCache {
+		t.Error("foo found in cache, and it shouldn't be there")
+	}
+
+	err = testRedisCache.Set("foo", "bar")
+	if err != nil {
+		t.Error(err)
+	}
+
+	inCache, err = testRedisCache.Has("foo")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !inCache {
+		t.Error("foo not found in cache, but it should be there")
+	}
+}
