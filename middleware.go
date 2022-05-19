77eb4676f56696e3a6475c5139c8d5c799a463da
petrostrak <pit.trak@gmail.com>
2022-05-19 17:44:49 +0300

    Add ExemptFlob in NoSurf

--- middleware.go
+++ middleware.go
@@ -16,6 +16,8 @@ func (s *Sokudo) NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 	secure, _ := strconv.ParseBool(s.config.cookie.secure)
 
+	csrfHandler.ExemptGlob("/api/*")
+
 	csrfHandler.SetBaseCookie(http.Cookie{
 		HttpOnly: true,
 		Path:     "/",
