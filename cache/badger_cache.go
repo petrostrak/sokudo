02a90b85e67c1154067ee2a41299ce0328f2ff0c
petrostrak <pit.trak@gmail.com>
2022-05-20 16:48:51 +0300

    Add emptyByMatch method

--- cache/badger_cache.go
+++ cache/badger_cache.go
@@ -95,3 +95,55 @@ func (b *BadgerCache) Empty() error {
 
 	return nil
 }
+
+func (b *BadgerCache) emptyByMatch(s string) error {
+	deleteKeys := func(keysForDelete [][]byte) error {
+		if err := b.Conn.Update(func(txn *badger.Txn) error {
+			for _, key := range keysForDelete {
+				if err := txn.Delete(key); err != nil {
+					return nil
+				}
+			}
+
+			return nil
+		}); err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	collectSize := 100000
+
+	err := b.Conn.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.AllVersions = false
+		opts.PrefetchValues = false
+		it := txn.NewIterator(opts)
+
+		keysForDelete := make([][]byte, 0, collectSize)
+		keysCollected := 0
+
+		for it.Seek([]byte(s)); it.ValidForPrefix([]byte(s)); it.Next() {
+			key := it.Item().KeyCopy(nil)
+			keysForDelete = append(keysForDelete, key)
+			keysCollected++
+
+			if keysCollected == collectSize {
+				if err := deleteKeys(keysForDelete); err != nil {
+					return err
+				}
+			}
+		}
+
+		if keysCollected > 0 {
+			if err := deleteKeys(keysForDelete); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+
+	return err
+}
