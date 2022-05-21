4064abab65b0aa4959a13469954824fb0897084e
petrostrak <pit.trak@gmail.com>
2022-05-21 16:47:03 +0300

    Updat setup_test.go to create a Badger database for testing

--- cache/setup_test.go
+++ cache/setup_test.go
@@ -1,15 +1,20 @@
 package cache
 
 import (
+	"log"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/alicebob/miniredis/v2"
+	"github.com/dgraph-io/badger/v3"
 	"github.com/gomodule/redigo/redis"
 )
 
-var testRedisCache RedisCache
+var (
+	testRedisCache  RedisCache
+	testBadherCache BadgerCache
+)
 
 func TestMain(m *testing.M) {
 	s, err := miniredis.Run()
@@ -31,6 +36,24 @@ func TestMain(m *testing.M) {
 	testRedisCache.Prefix = "test-sokudo"
 
 	defer testRedisCache.Conn.Close()
+
+	_ = os.RemoveAll("./testdata/tmp/badger")
+
+	// create a badger DB
+	if _, err := os.Stat("./testdata/tmp"); os.IsNotExist(err) {
+		err := os.Mkdir("./testdata/tmp", 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	err = os.Mkdir("./testdata/tmp/badger", 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, _ := badger.Open(badger.DefaultOptions("./testdata/tmp/badger"))
+	testBadherCache.Conn = db
 
 	os.Exit(m.Run())
 }
