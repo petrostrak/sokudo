65458710b0b6b67154d75b917d489c5f1a6f15e4
petrostrak <pit.trak@gmail.com>
2022-05-23 17:36:50 +0300

    Sanitize the project name

--- cmd/cli/new.go
+++ cmd/cli/new.go
@@ -1,11 +1,20 @@
 package main
 
-import "strings"
+import (
+	"log"
+	"strings"
+)
 
 func doNew(appName string) {
-	name := strings.ToLower(appName)
+	appName = strings.ToLower(appName)
 
 	// sanitize the application name (convert url to single word)
+	if strings.Contains(appName, "/") {
+		exploded := strings.SplitAfter(appName, "/")
+		appName = exploded[len(exploded)-1]
+	}
+
+	log.Println("App name is", appName)
 
 	// git clone the skeleton application
 
