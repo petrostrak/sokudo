8255232136c79cf81bef9d8c4fa04bcef8d75d60
petrostrak <pit.trak@gmail.com>
2022-05-23 17:32:07 +0300

    Add new case

---
+++ cmd/cli/new.go
@@ -0,0 +1,23 @@
+package main
+
+import "strings"
+
+func doNew(appName string) {
+	name := strings.ToLower(appName)
+
+	// sanitize the application name (convert url to single word)
+
+	// git clone the skeleton application
+
+	// remove the .git directory
+
+	// create a ready to go .env file
+
+	// create a Makefile
+
+	// update the go.mod file
+
+	// update existing .go files with correct name/imports
+
+	// run go mod tidy in the project directory
+}
