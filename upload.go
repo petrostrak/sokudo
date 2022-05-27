772038a1c2a587749a5d7d38ecb65369ba0f97fa
petrostrak <pit.trak@gmail.com>
2022-05-27 15:49:16 +0300

    Create the file uploader

---
+++ upload.go
@@ -0,0 +1,62 @@
+package sokudo
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+
+	"github.com/petrostrak/sokudo/filesystems"
+)
+
+// For the last parameter fs, we want to pass a pointer to a FS if we want to upload to any
+// remote FS(minio, s3, webDav or sftp) or nil for uploading to the local FS on the server.
+func (s *Sokudo) UploadFile(r *http.Request, dst, field string, fs filesystems.FS) error {
+	fileName, err := s.getFileToUpload(r, field)
+	if err != nil {
+		s.ErrorLog.Println(err)
+		return err
+	}
+
+	if fs != nil {
+		if err = fs.Put(fileName, dst); err != nil {
+			s.ErrorLog.Println(err)
+			return err
+		}
+	} else {
+		// upload to the local filesystem on the server
+		if err = os.Rename(fileName, fmt.Sprintf("%s/%s", dst, path.Base(fileName))); err != nil {
+			s.ErrorLog.Println(err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *Sokudo) getFileToUpload(r *http.Request, fieldName string) (string, error) {
+	err := r.ParseMultipartForm(10 << 22)
+	if err != nil {
+		return "", err
+	}
+
+	file, header, err := r.FormFile(fieldName)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	dst, err := os.Create(fmt.Sprintf("./tmp/%s", header.Filename))
+	if err != nil {
+		return "", err
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("./tmp/%s", header.Filename), nil
+}
